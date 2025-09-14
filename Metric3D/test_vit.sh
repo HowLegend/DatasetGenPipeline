4103103e36467bb2d8362c1e0af2e8185f8b3c93
Junhao <2783324881@qq.com>
2025-09-14 10:07:58 +0800

    删除了三个开源库，请自行下载

--- Metric3D/test_vit.sh
+++
@@ -1,5 +0,0 @@
-python mono/tools/test_scale_cano.py \
-    'mono/configs/HourglassDecoder/vit.raft5.large.py' \
-    --load-from ./weight/metric_depth_vit_large_800k.pth \
-    --test_data_path ./data/wild_demo \
-    --launcher None
